379d749f4ea9e7e8828d50c02f4704227b737cbc
Sam Kaiser <samuel.k.public@gmail.com>
2026-03-11 17:16:06 -0500

    Assignment 6: display discounts that are currently valid within a Boostrap list group

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,4 +11,11 @@ public class ProductController(DataContext db) : Controller
         var products = _dataContext.Products.Where(product => product.CategoryId.Equals(id)).Where(product => product.Discontinued.Equals(false));
         return View(products);
     }
+
+    public IActionResult Discounts()
+    {
+        var discounts = _dataContext.Discounts
+            .Where(d => d.StartTime <= DateTime.Now && d.EndTime >= DateTime.Now);
+        return View(discounts);
+    }
 }
